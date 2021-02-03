89718d4e87623f681c215b9ffda131c1d353b846
Shahnawaz Alam <shahnawazshafiss9@gmail.com>
2021-02-03 17:57:46 +0530

    continuewith83

---
+++ src/component/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div class="ui container">
+                <form>
+<div className="ui input">
+  <input type="text" placeholder="Search..." />
+</div>
+                </form>
+            </div>
+        )
+    }
+}
+export default SearchBar;
